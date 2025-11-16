db49da07f400e0e040003ffb5433209545626504
WasepSKT <wasep.aziz@skt-global.co.id>
2025-11-17 05:26:29 +0700

    update backend

--- be-payment-shipment-service/src/clients/xenditClient.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-import { config } from '../config';
-
-export const xenditClient = axios.create({
-  baseURL: 'https://api.xendit.co',
-  auth: { username: config.xendit.secretKey, password: '' },
-  headers: { 'Content-Type': 'application/json' },
-});
